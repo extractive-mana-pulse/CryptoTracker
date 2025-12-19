e6be16a83b438d0ccf004ca9ed7da89fd3155a90
Mukhammadamin S. <96365967+extractive-mana-pulse@users.noreply.github.com>
2025-12-20 00:09:22 +0500

    Coin List View Model implemented and ready to setup (#4)

---
+++ app/src/main/java/com/plcoding/cryptotracker/cryto/presentation/coin_list/CoinListViewModel.kt
@@ -0,0 +1,56 @@
+package com.plcoding.cryptotracker.cryto.presentation.coin_list
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.plcoding.cryptotracker.core.domain.util.onError
+import com.plcoding.cryptotracker.core.domain.util.onSuccess
+import com.plcoding.cryptotracker.cryto.domain.CoinDataSource
+import com.plcoding.cryptotracker.cryto.presentation.model.toCoinUiState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+class CoinListViewModel(
+    private val coinDataSource: CoinDataSource
+): ViewModel() {
+
+    private val _state = MutableStateFlow(CoinListState())
+    val state = _state
+        .onStart { loadCoins() }
+        .stateIn(
+            viewModelScope,
+            SharingStarted.WhileSubscribed(5000L),
+            CoinListState()
+        )
+
+    fun onAction(action: CoinListAction) {
+        when(action) {
+            is CoinListAction.OnCoinClick -> {
+
+            }
+        }
+    }
+    private fun loadCoins() {
+        viewModelScope.launch {
+            _state.update { it.copy(
+                isLoading = true
+            ) }
+
+            coinDataSource
+                .getCoins()
+                .onSuccess { coins ->
+                    _state.update { it.copy(
+                        isLoading = false,
+                        coins = coins.map { it.toCoinUiState() }
+                    ) }
+                }
+                .onError { error ->
+                    _state.update { it.copy(isLoading = false) }
+                }
+        }
+    }
+}
