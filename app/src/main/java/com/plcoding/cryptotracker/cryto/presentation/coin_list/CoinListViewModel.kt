312f35ed59c0f9bd60df62ee8a607e06e5af5ca8
Mukhammadamin S. <96365967+extractive-mana-pulse@users.noreply.github.com>
2026-01-03 20:58:08 +0500

    Network Error handling implemented (#6)

--- app/src/main/java/com/plcoding/cryptotracker/cryto/presentation/coin_list/CoinListViewModel.kt
+++ app/src/main/java/com/plcoding/cryptotracker/cryto/presentation/coin_list/CoinListViewModel.kt
@@ -7,10 +7,12 @@ import com.plcoding.cryptotracker.core.domain.util.onError
 import com.plcoding.cryptotracker.core.domain.util.onSuccess
 import com.plcoding.cryptotracker.cryto.domain.CoinDataSource
 import com.plcoding.cryptotracker.cryto.presentation.model.toCoinUiState
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
@@ -28,6 +30,9 @@ class CoinListViewModel(
             CoinListState()
         )
 
+    private val _events = Channel<CoinListEvent>()
+    val events = _events.receiveAsFlow()
+
     fun onAction(action: CoinListAction) {
         when(action) {
             is CoinListAction.OnCoinClick -> {
@@ -35,18 +40,25 @@ class CoinListViewModel(
             }
         }
     }
+
     private fun loadCoins() {
         viewModelScope.launch {
-            _state.update { it.copy(isLoading = true) }
+            _state.update { it.copy(
+                isLoading = true
+            ) }
 
-            coinDataSource.getCoins().onSuccess { coins ->
-                // Log the received coins
-                Log.d("CoinListViewModel", "Received coins: $coins")
-                _state.update { it.copy(isLoading = false, coins = coins.map { it.toCoinUiState() }) }
-            }.onError { error ->
-                Log.e("CoinListViewModel", "Error fetching coins: $error")
-                _state.update { it.copy(isLoading = false) }
-            }
+            coinDataSource
+                .getCoins()
+                .onSuccess { coins ->
+                    _state.update { it.copy(
+                        isLoading = false,
+                        coins = coins.map { it.toCoinUiState() }
+                    ) }
+                }
+                .onError { error ->
+                    _state.update { it.copy(isLoading = false) }
+                    _events.send(CoinListEvent.Error(error))
+                }
         }
     }
 }
