b200e73f65f6c9bd92f5ded9734e63fb10084882
extractive-mana-pulse <invoker1441@gmail.com>
2026-01-21 10:02:29 +0500

    v1.1.1 fixed, now it's v1.1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 1
-        versionName = "1.1.1"
+        versionName = "1.1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
